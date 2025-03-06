0373570f5c228d30ba7bc07bf10b78168f75e5f4
Saul Lee <solleedata@gmail.com>
2025-03-06 18:12:18 +0900

    refactor: destructure item prop in MenuItemCard component

--- app/features/menu/item.tsx
+++ app/features/menu/item.tsx
@@ -1,7 +1,7 @@
 import { Box, Txt } from '@saul-atomrigs/design-system';
 import type { MenuItem } from '~/remotes';
 
-export function MenuItemCard(item: MenuItem) {
+export function MenuItemCard({ item }: { item: MenuItem }) {
   return (
     <Box
       style={{

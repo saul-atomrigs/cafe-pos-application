75be472fa5b00e28a1136a2d0df37ace574267af
Saul Lee <solleedata@gmail.com>
2025-03-07 13:30:10 +0900

    refactor: add AsyncBoundary for error and loading handling in StartPage

--- app/features/start/page.tsx
+++ app/features/start/page.tsx
@@ -1,4 +1,5 @@
-import { CTAButton, Txt } from '@saul-atomrigs/design-system';
+import { CTAButton, Error, Loading, Txt } from '@saul-atomrigs/design-system';
+import { AsyncBoundary } from '@toss/async-boundary';
 import { useNavigate } from 'react-router';
 import { ROUTES } from '~/routes';
 
@@ -6,14 +7,21 @@ export default function StartPage() {
   const navigate = useNavigate();
 
   return (
-    <div className='container'>
-      <Txt size='xl' weight='bold'>
-        POS에 오신걸 환영합니다
-      </Txt>
+    <AsyncBoundary
+      rejectedFallback={() => (
+        <Error message='시작 화면을 불러오지 못했습니다' />
+      )}
+      pendingFallback={<Loading message='시작 화면을 불러오고 있습니다' />}
+    >
+      <div className='container'>
+        <Txt size='xl' weight='bold'>
+          POS에 오신걸 환영합니다
+        </Txt>
 
-      <CTAButton onClick={() => navigate(ROUTES.MENU)}>
-        메뉴로 이동하기
-      </CTAButton>
-    </div>
+        <CTAButton onClick={() => navigate(ROUTES.MENU)}>
+          메뉴로 이동하기
+        </CTAButton>
+      </div>
+    </AsyncBoundary>
   );
 }

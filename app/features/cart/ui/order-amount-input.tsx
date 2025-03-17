ff342f60fda88c2e8f61c04a9b0d3f848593ca7c
Saul Lee <solleedata@gmail.com>
2025-03-17 12:17:04 +0900

    feat: set default quantity for order amount input to improve cart functionality

--- app/features/cart/ui/order-amount-input.tsx
+++ app/features/cart/ui/order-amount-input.tsx
@@ -2,23 +2,25 @@ import { Button, Txt } from '@saul-atomrigs/design-system';
 import { useCartContext } from '../context';
 import type { MenuItem } from '~/remotes';
 
+const DEFAULT_QUANTITY = 1;
+
 export default function OrderAmountInput({ item }: { item: MenuItem }) {
   const { cartItems, addToCart, updateQuantity } = useCartContext();
   const cartItem = cartItems.find((cartItem) => cartItem.item.id === item.id);
 
-  const amount = cartItem ? cartItem.quantity : 0;
+  const amount = cartItem ? cartItem.quantity : DEFAULT_QUANTITY;
 
   const handleIncrement = () => {
     if (amount === 0) {
-      addToCart(item, 1);
+      addToCart(item, DEFAULT_QUANTITY);
     } else {
-      updateQuantity(item.id, amount + 1);
+      updateQuantity(item.id, amount + DEFAULT_QUANTITY);
     }
   };
 
   const handleDecrement = () => {
     if (amount > 0) {
-      updateQuantity(item.id, amount - 1);
+      updateQuantity(item.id, amount - DEFAULT_QUANTITY);
     }
   };
   return (
